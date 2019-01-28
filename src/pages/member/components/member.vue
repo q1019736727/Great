c8539daceb92221f7093b763400d0ebe7a5b4280
Chiu Yung <1837060380@qq.com>
2019-01-28 17:49:12 +0800

    no message

---
+++ src/pages/member/components/member.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container " style="min-height: 581px;">
+    <div class="content">
+      <div class="user-infos">
+        <div class="pull-left avatar">
+          <div class="img" style="background-image: url(https://img.yzcdn.cn/upload_files/no_pic.png?imageView2/2/w/280/h/280/q/75/format/webp);"></div>
+        </div>
+        <div class="pull-left desc">
+          <p>tonyfree</p>
+        </div>
+      </div>
+      <div class="order-related">
+        <ul class="uc-order list-horizon clearfix js-order-overview">
+          <li>
+            <a class="link clearfix relative link-topay" data-for="topay" target="_blank">
+              <p class="title-info c-black font-size-12">待付款</p>
+            </a>
+          </li>
+          <li>
+            <a class="link clearfix relative link-totuan" data-for="totuan" target="_blank">
+              <p class="title-info c-black font-size-12">待接单</p>
+            </a>
+          </li>
+          <li>
+            <a class="link clearfix relative link-tosend" data-for="tosend"  target="_blank">
+              <p class="title-info c-black font-size-12">待发货</p>
+            </a>
+          </li>
+          <li>
+            <a class="link clearfix relative link-send" data-for="send"  target="_blank">
+              <p class="title-info c-black font-size-12">待收货</p>
+            </a>
+          </li>
+          <li>
+            <a class="link clearfix relative link-rights" target="_blank">
+              <p class="title-info c-black font-size-12">退款/维权</p>
+            </a>
+          </li>
+        </ul>
+        <div class="block block-list list-vertical">
+          <router-link class="block-item link clearfix ico-order" to="/adress" target="_self">
+            <p class="title-info c-black font-size-14">收货地址</p>
+          </router-link>
+        </div>
+        <div class="block block-list list-vertical">
+          <a class="block-item link clearfix ico-favior js-login"  target="_blank">
+            <p class="title-info c-black font-size-14">收藏的商品</p>
+          </a>
+          <a class="block-item link clearfix ico-shop js-login"  target="_blank">
+            <p class="title-info c-black font-size-14">收藏的店铺</p>
+          </a>
+          <a class="block-item link clearfix ico-bought js-login"  target="_blank">
+            <p class="title-info c-black font-size-14">我买过的</p>
+          </a>
+          <a class="block-item link clearfix ico-history js-login"  target="_blank">
+            <p class="title-info c-black font-size-14">我的浏览记录</p>
+          </a>
+        </div>
+        <div class="block block-list list-vertical">
+          <a class="block-item link clearfix ico-card"  target="_blank">
+            <p class="title-info c-black font-size-14">我的会员卡</p>
+          </a>
+          <a class="block-item link clearfix ico-coupon"  target="_blank">
+            <p class="title-info c-black font-size-14">我的优惠券</p>
+          </a>
+          <a class="block-item link clearfix ico-luckymoney hide"  target="_blank">
+            <p class="title-info c-black font-size-14">我的红包</p>
+          </a>
+          <a class="block-item link clearfix ico-backs"  target="_blank">
+            <p class="title-info c-black font-size-14">我的返现</p>
+          </a>
+        </div>
+        <div class="block block-list list-vertical">
+          <a class="block-item link clearfix ico-setting js-login"  target="_blank">
+            <p class="title-info c-black font-size-14">个人设置</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/member/member.js
+++ src/pages/member/member.js
@@ -1,3 +1,29 @@
 
 import './index_base.css'
 import './index.css'
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+  routes:[
+    {
+      path:'/',
+      name:'member',
+      component:require('./components/member.vue')
+    },
+    {
+      path:'/adress',
+      name:'adress',
+      component:require('./components/allAddress.vue')
+    },
+
+  ]
+})
+
+new Vue({
+  el:'#app',
+  router,
+
+})
